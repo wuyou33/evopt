f5458d390272b2f861243fbdbcebe00216290c88
DQQ-077-SZ <duanqq257@qq.com>
2018-08-21 20:56:22 +0800

    test CMAES on cfSphere

--- matlab/test/algo/cont-opt/stochastic-opt/CMAES/testPureCMAES_cfSphere.m
+++ matlab/test/algo/cont-opt/stochastic-opt/CMAES/testPureCMAES_cfSphere.m
@@ -6,10 +6,12 @@ clear; clc;
 conFuncParams = ConFuncParams('cfSphere', 100, 100);
 optAlgoParams = OptAlgoParams('PureCMAES', 5e6, 1);
 testParams = TestParams(25, true, 24);
-RunAlgo(conFuncParams, testParams, optAlgoParams);
+optRes = RunAlgo(conFuncParams, testParams, optAlgoParams);
+disp(length(optRes));
 
 %%
 conFuncParams = ConFuncParams('cfSphere', 1000, 100);
 optAlgoParams = OptAlgoParams('PureCMAES', 5e6, 1);
 testParams = TestParams(25, true, 52);
-RunAlgo(conFuncParams, testParams, optAlgoParams);
+optRes = RunAlgo(conFuncParams, testParams, optAlgoParams);
+save('PureCMAES_cfSphere.mat', 'optRes');
