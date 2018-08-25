7e9008c15196b0bf162748d088b5b08a1ef5db60
DQQ-077-SZ <duanqq257@qq.com>
2018-08-25 18:52:34 +0800

    update

--- matlab/test/algo/cont-opt/large-scale-opt/DECCG/compDECCG.m
+++ matlab/test/algo/cont-opt/large-scale-opt/DECCG/compDECCG.m
@@ -14,68 +14,203 @@ meanDECCG = [2.17e-25 5.37e-14 3.71e-23 1.01e-01 9.87e+02 0.00e+00 8.40e-03 ...
 %%
 % NOTE that we repeated the experiments of the above paper, and could
 %   obtain the below data. Luckly, we obtained the same or slightly
-%   better results in most cases, except f03 (due to unknown reasons)
-%   and f13 (perhaps due to randomness).
-meanDECCGRepeat = Inf * ones(1, length(funcInds));
-curves = zeros(50000, length(funcInds));
-optResFolder = 'CentOS7'; % run on CentOS 7
-for f = 1 : length(funcInds)
-    optResFilename = sprintf('%s/testDECCG_f%02d.mat', optResFolder, f);
-    load(optResFilename);
-    meanDECCGRepeat(f) = mean(optys);
-    curveLength = Inf * ones(1, testMax);
-    for t = 1 : testMax
-        curveLength(t) = length(funcEvalCurves{t});
-        if curveLength(t) > size(curves, 1) % unexpected length
-            fprintf(sprintf('*** Warning ***: %02d - %02d.', f, t));
-        end
-        curves(1 : curveLength(t), f) = ...
-            curves(1 : curveLength(t), f) + funcEvalCurves{t};
-    end
-    minCurveLength = min(curveLength);
-    curves(1 : minCurveLength, f) = curves(1 : minCurveLength, f) / testMax;
-    curves((minCurveLength + 1) : end, f) = Inf;
-end
+%   better results in most cases, except f03 (due to unknown reasons).
+% optResFolder = 'testDECCG_CentOS7'; % run on CentOS 7
+% for f = 1 : length(funcInds)
+%     optResFilename = sprintf('%s/testDECCG_f%02d.mat', optResFolder, f);
+%     load(optResFilename);
+%     funcEvalAvg = zeros(5000000, 1);
+%     curveLength = Inf * ones(1, testMax);
+%     for t = 1 : testMax
+%         funcEvalCurve = funcEvalCurves{t};
+%         curveLength(t) = length(funcEvalCurve);
+%         for c = 2 : length(funcEvalCurve)
+%             if funcEvalCurve(c - 1) < funcEvalCurve(c)
+%                 funcEvalCurve(c) = funcEvalCurve(c - 1);
+%             end
+%         end
+%         if curveLength(t) >= size(funcEvalAvg, 1) % unexpected length
+%             funcEvalCurve = funcEvalCurve(1 : size(funcEvalAvg, 1));
+%         else
+%             error('ERROR!');
+%         end
+%         funcEvalAvg = funcEvalAvg + funcEvalCurve;
+%     end
+%     funcEvalAvg = funcEvalAvg / testMax;
+%     save(optResFilename, 'optys', 'optxs', 'funcEvalAvg');
+% end
 
 %%
 % NOTE that here only the cooperative co-evolution part is run (i.e.,
-%   without adaptive weights).
-meanDECCGRepeat_ = Inf * ones(1, length(funcInds));
-curves_ = zeros(50000, length(funcInds));
-optResFolder_ = 'CentOS7waw'; % run on CentOS 7
-for f = 1 : length(funcInds)
-    optResFilename = sprintf('%s/testDECCG_f%02d.mat', optResFolder_, f);
-    load(optResFilename);
-    meanDECCGRepeat_(f) = mean(optys);
-    curveLength = Inf * ones(1, testMax);
-    for t = 1 : testMax
-        curveLength(t) = length(funcEvalCurves{t});
-        if curveLength(t) > size(curves_, 1) % unexpected length
-            fprintf(sprintf('*** Warning ***: %02d - %02d.', f, t));
-        end
-        curves_(1 : curveLength(t), f) = ...
-            curves_(1 : curveLength(t), f) + funcEvalCurves{t};
-    end
-    minCurveLength = min(curveLength);
-    curves_(1 : minCurveLength, f) = curves_(1 : minCurveLength, f) / testMax;
-    curves_((minCurveLength + 1) : end, f) = Inf;
-end
+%   without adaptive weights -> waw).
+% optResFolder = 'testDECCG_CentOS7_waw'; % run on CentOS 7
+% for f = 1 : length(funcInds)
+%     optResFilename = sprintf('%s/testDECCG_f%02d.mat', optResFolder, f);
+%     load(optResFilename);
+%     funcEvalAvg_ = zeros(5000000, 1);
+%     curveLength = Inf * ones(1, testMax);
+%     for t = 1 : testMax
+%         funcEvalCurve = funcEvalCurves{t};
+%         curveLength(t) = length(funcEvalCurve);
+%         for c = 2 : length(funcEvalCurve)
+%             if funcEvalCurve(c - 1) < funcEvalCurve(c)
+%                 funcEvalCurve(c) = funcEvalCurve(c - 1);
+%             end
+%         end
+%         if curveLength(t) >= size(funcEvalAvg_, 1) % unexpected length
+%             funcEvalCurve = funcEvalCurve(1 : size(funcEvalAvg_, 1));
+%         else
+%             error('ERROR!');
+%         end
+%         funcEvalAvg_ = funcEvalAvg_ + funcEvalCurve;
+%     end
+%     funcEvalAvg_ = funcEvalAvg_ / testMax;
+%     save(optResFilename, 'optys', 'optxs', 'funcEvalAvg_');
+% end
 
 %%
-meanComp = [meanDECCG; meanDECCGRepeat; meanDECCGRepeat_];
+% L1 metric.
+% optResFolder = 'testDECCG_L1_CentOS7'; % run on CentOS 7
+% for f = 1 : length(funcInds)
+%     optResFilename = sprintf('%s/testDECCG_f%02d.mat', optResFolder, f);
+%     load(optResFilename);
+%     funcEvalAvg = zeros(5000000, 1);
+%     curveLength = Inf * ones(1, testMax);
+%     for t = 1 : testMax
+%         funcEvalCurve = funcEvalCurves{t};
+%         curveLength(t) = length(funcEvalCurve);
+%         for c = 2 : length(funcEvalCurve)
+%             if funcEvalCurve(c - 1) < funcEvalCurve(c)
+%                 funcEvalCurve(c) = funcEvalCurve(c - 1);
+%             end
+%         end
+%         if curveLength(t) >= size(funcEvalAvg, 1) % unexpected length
+%             funcEvalCurve = funcEvalCurve(1 : size(funcEvalAvg, 1));
+%         else
+%             error('ERROR!');
+%         end
+%         funcEvalAvg = funcEvalAvg + funcEvalCurve;
+%     end
+%     funcEvalAvg = funcEvalAvg / testMax;
+%     save(optResFilename, 'optys', 'optxs', 'funcEvalAvg');
+% end
+
+%%
+% L1 metric (without adaptive weights -> waw).
+% optResFolder = 'testDECCG_L1_CentOS7_waw'; % run on CentOS 7
+% for f = 1 : length(funcInds)
+%     optResFilename = sprintf('%s/testDECCG_f%02d.mat', optResFolder, f);
+%     load(optResFilename);
+%     funcEvalAvg_ = zeros(5000000, 1);
+%     curveLength = Inf * ones(1, testMax);
+%     for t = 1 : testMax
+%         funcEvalCurve = funcEvalCurves{t};
+%         curveLength(t) = length(funcEvalCurve);
+%         for c = 2 : length(funcEvalCurve)
+%             if funcEvalCurve(c - 1) < funcEvalCurve(c)
+%                 funcEvalCurve(c) = funcEvalCurve(c - 1);
+%             end
+%         end
+%         if curveLength(t) >= size(funcEvalAvg_, 1) % unexpected length
+%             funcEvalCurve = funcEvalCurve(1 : size(funcEvalAvg_, 1));
+%         else
+%             error('ERROR!');
+%         end
+%         funcEvalAvg_ = funcEvalAvg_ + funcEvalCurve;
+%     end
+%     funcEvalAvg_ = funcEvalAvg_ / testMax;
+%     save(optResFilename, 'optys', 'optxs', 'funcEvalAvg_');
+% end
+
+%%
+meanDECCGRepeat = Inf * ones(1, length(funcInds));
+meanDECCGRepeat_ = Inf * ones(1, length(funcInds));
+meanDECC_L1 = Inf * ones(1, length(funcInds));
+meanDECC_L1_ = Inf * ones(1, length(funcInds));
+for f = 1 : length(funcInds)
+    optResFilename = sprintf('testDECCG_CentOS7/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    meanDECCGRepeat(f) = mean(optys);
+    optResFilename = sprintf('testDECCG_CentOS7_waw/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    meanDECCGRepeat_(f) = mean(optys);
+    optResFilename = sprintf('testDECCG_L1_CentOS7/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    meanDECC_L1(f) = mean(optys);
+    optResFilename = sprintf('testDECCG_L1_CentOS7_waw/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    meanDECC_L1_(f) = mean(optys);
+end
+
+meanComp = [meanDECCG; ...
+    meanDECCGRepeat; meanDECCGRepeat_; ...
+    meanDECC_L1; meanDECC_L1_];
+
+%%
+% xFolder = 'testDECCG_CentOS7/testDECCG_f%02d.mat';
+% yFolder = 'testDECCG_CentOS7_waw/testDECCG_f%02d.mat';
+
+% xFolder = 'testDECCG_CentOS7/testDECCG_f%02d.mat';
+% yFolder = 'testDECCG_L1_CentOS7/testDECCG_f%02d.mat';
+
+% xFolder = 'testDECCG_CentOS7_waw/testDECCG_f%02d.mat';
+% yFolder = 'testDECCG_L1_CentOS7_waw/testDECCG_f%02d.mat';
+%  4: 8.27275261708908e-56 (70.3994008303297    [waw] vs. 8.94175059259894e-05 [L1waw])
+%  5: 9.04340984391320e-45 (2792.15221403624    [waw] vs. 985.730207896399     [L1waw])
+% 10: 2.35654077923863e-40 (1.19771387034034    [waw] vs. 1.03420727022296e-07 [L1waw])
+% 11: 0.0198498501077987   (0.00482572095032130 [waw] vs. 1.33795197143627e-13 [L1waw])
+
+% xFolder = 'testDECCG_L1_CentOS7/testDECCG_f%02d.mat';
+% yFolder = 'testDECCG_L1_CentOS7_waw/testDECCG_f%02d.mat';
+
+% xFolder = 'testDECCG_L1_CentOS7/testDECCG_f%02d.mat';
+% yFolder = 'testDECCG_CentOS7_waw/testDECCG_f%02d.mat';
+
+ttestComp = Inf * ones(2, length(funcInds));
+ttest2Comp = Inf * ones(2, length(funcInds));
+for f = 1 : length(funcInds)
+    optResFilename = sprintf(xFolder, f);
+    load(optResFilename);
+    x = optys;
+    optResFilename = sprintf(yFolder, f);
+    load(optResFilename);
+    y = optys;
+    [ttestComp(1, f), ttestComp(2, f)] = ttest(x, y);
+    [ttest2Comp(1, f), ttest2Comp(2, f)] = ttest2(x, y);
+end
 
 %%
 for f = 1 : length(funcInds)
     figure(f);
-    plot(curves(:, f)); hold on;
-    plot(curves_(:, f));
-    legend('DECCG', 'DECCGwaw');
+    optResFilename = sprintf('testDECCG_CentOS7/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    plot(funcEvalAvg); hold on;
+    optResFilename = sprintf('testDECCG_CentOS7_waw/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    plot(funcEvalAvg_); hold on;
+    optResFilename = sprintf('testDECCG_L1_CentOS7/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    plot(funcEvalAvg); hold on;
+    optResFilename = sprintf('testDECCG_L1_CentOS7_waw/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    plot(funcEvalAvg_); hold on;
+    legend('DECCG', 'DECCGwaw', 'DECCG-L1', 'DECCG-L1waw');
 end
 
 %%
 for f = 1 : length(funcInds)
     figure(100 + f);
-    semilogy(curves(:, f)); hold on;
-    semilogy(curves_(:, f));
-    legend('DECCG', 'DECCGwaw');
+    optResFilename = sprintf('testDECCG_CentOS7/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    semilogy(funcEvalAvg); hold on;
+    optResFilename = sprintf('testDECCG_CentOS7_waw/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    semilogy(funcEvalAvg_);
+    optResFilename = sprintf('testDECCG_L1_CentOS7/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    semilogy(funcEvalAvg); hold on;
+    optResFilename = sprintf('testDECCG_L1_CentOS7_waw/testDECCG_f%02d.mat', f);
+    load(optResFilename);
+    semilogy(funcEvalAvg_); hold on;
+    legend('DECCG', 'DECCGwaw', 'DECCG-L1', 'DECCG-L1waw');
 end
