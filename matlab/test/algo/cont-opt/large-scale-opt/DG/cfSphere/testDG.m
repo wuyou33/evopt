2bc794c5b4a555166b1e25d808485871865f0090
DQQ-077-SZ <duanqq257@qq.com>
2018-08-30 20:25:42 +0800

    test *DG*

---
+++ matlab/test/algo/cont-opt/large-scale-opt/DG/cfSphere/testDG.m
@@ -0,0 +1,30 @@
+clear; clc;
+
+% test correction of the program *lsoDG*
+
+%%
+funcDim = 2;
+conFuncParams = ConFuncParams('cfSphere', funcDim, 10, -10);
+[nonSepDims, sepDims, funcEvalNum] = lsoDG(conFuncParams, 1e-3);
+disp(length(nonSepDims)); % 0
+disp(length(sepDims)); % 2
+disp(all(sepDims == 1 : funcDim)); % 1
+disp(funcEvalNum); % 6
+
+%%
+funcDim = 100;
+conFuncParams = ConFuncParams('cfSphere', funcDim, 10, -10);
+[nonSepDims, sepDims, funcEvalNum] = lsoDG(conFuncParams, 1e-3);
+disp(length(nonSepDims)); % 0
+disp(length(sepDims)); % 100
+disp(all(sepDims == 1 : funcDim)); % 1
+disp(funcEvalNum); % 10100
+
+%%
+funcDim = 1000;
+conFuncParams = ConFuncParams('cfSphere', funcDim, 100, -100);
+[nonSepDims, sepDims, funcEvalNum] = lsoDG(conFuncParams, 1e-3);
+disp(length(nonSepDims)); % 0
+disp(length(sepDims)); % 1000
+disp(all(sepDims == 1 : funcDim)); % 1
+disp(funcEvalNum); % 1001000
---
+++ matlab/test/algo/cont-opt/large-scale-opt/DG/cfSphere/testDG_.m
@@ -0,0 +1,41 @@
+clear; clc;
+
+% NOTE that put this code in the folder which contains the file *dg.m*.
+% Refer to * https://bitbucket.org/mno/differential-grouping *
+% for the original MATLAB source code.
+
+%%
+fun = @(X, y) (feval('cfSphere', X));
+options.ubound = 10;
+options.lbound = -10;
+options.dim = 2;
+options.epsilon = 1e-3;
+[seps, allgroups, FEs] = dg(fun, 1, options);
+disp(length(allgroups)); % 0
+disp(length(seps)); % 2
+disp(all(seps' == 1 : options.dim)); % 1
+disp(FEs); % 6
+
+%%
+fun = @(X, y) (feval('cfSphere', X));
+options.ubound = 10;
+options.lbound = -10;
+options.dim = 100;
+options.epsilon = 1e-3;
+[seps, allgroups, FEs] = dg(fun, 1, options);
+disp(length(allgroups)); % 0
+disp(length(seps)); % 100
+disp(all(seps' == 1 : options.dim)); % 1
+disp(FEs); % 10100
+
+%%
+fun = @(X, y) (feval('cfSphere', X));
+options.ubound = 100;
+options.lbound = -100;
+options.dim = 1000;
+options.epsilon = 1e-3;
+[seps, allgroups, FEs] = dg(fun, 1, options);
+disp(length(allgroups)); % 0
+disp(length(seps)); % 1000
+disp(all(seps' == 1 : options.dim)); % 1
+disp(FEs); % 1001000
